5eec2549f7c66b0f593b9c06df2be91c1fd27ce3
Sukhbat05 <asukhbat27@gmail.com>
2025-09-15 22:28:59 +0800

    hf3

--- src/components/MobileSearch.tsx
+++ src/components/MobileSearch.tsx
@@ -4,21 +4,37 @@ import { useState } from "react";
 import SearchInput from "./SearchInput";
 import { CiSearch } from "react-icons/ci";
 import { Button } from "./ui/button";
+import { tr } from "zod/v4/locales";
 
 export const MobileSearch = () => {
   const [isSearching, setIsSearching] = useState(false);
+
   const handleClick = () => {
-    setIsSearching(!isSearching);
+    setIsSearching((prev) => !prev);
   };
+
   return (
     <div className="hidden max-sm:block">
-      <Button variant="secondary" onClick={handleClick} className="block">
-        <CiSearch />
-      </Button>
-
-      {isSearching && (
-        <div className="max-sm:w-30">
-          <SearchInput />
+      {!isSearching ? (
+        <Button
+          variant="secondary"
+          onClick={handleClick}
+          className="block"
+          aria-label="Open search"
+        >
+          <CiSearch size={20} />
+        </Button>
+      ) : (
+        <div className="flex items-center w-35">
+           <SearchInput />
+         
+          <Button
+            variant="secondary"
+            onClick={handleClick}
+            aria-label="Close search"
+          >
+            ✕
+          </Button>
         </div>
       )}
     </div>
